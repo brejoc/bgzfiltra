689a6138fe1148aa19ac0b8cdeda6a580ae21d58
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2023-01-03 15:15:48 +0000

    Introduce fallback mechanism for bugzilla authentation
    If API KEYS are not valid, then we fallback to use legacy username
    and password method (if enabled).

--- src/bgznet/main.py
+++ src/bgznet/main.py
@@ -28,11 +28,20 @@ def get_bugs_for_product(
 
 
 def __get_bugzilla_api(bgz_settings: typing.Dict[str, str]) -> bugzilla.Bugzilla:
-    if bgz_settings["use_legacy_credentials"]:
-        bzapi = bugzilla.Bugzilla(bgz_settings["url"], sslverify=bgz_settings["sslverify"])
-        bzapi.login(user=bgz_settings["username"], password=bgz_settings["password"])
-    else:
-        bzapi = bugzilla.Bugzilla(bgz_settings["url"], api_key=bgz_settings["apikey"], sslverify=bgz_settings["sslverify"])
+    print("Trying to authenticate to Bugzilla")
+    bzapi = bugzilla.Bugzilla(bgz_settings["url"], api_key=bgz_settings["apikey"], sslverify=bgz_settings["sslverify"])
+    try:
+        if not bzapi.logged_in:
+            raise Exception("Authentication token is invalid or user is already logged out")
+    except Exception as exc:
+        if bgz_settings["use_legacy_credentials"]:
+            print(f"There was an error authenticating Bugzilla using API KEY: {exc}")
+            print("Trying authentication using legacy username and password")
+            bzapi = bugzilla.Bugzilla(bgz_settings["url"], sslverify=bgz_settings["sslverify"])
+            bzapi.login(user=bgz_settings["username"], password=bgz_settings["password"])
+        else:
+            raise exc
+    print("Bugzilla authentication was successful!")
     return bzapi
 
 
